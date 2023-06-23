0a7f4765fa30b15b5f636e3fee66d05a0e9e2e03
Serge Smetana <serge.smetana@gmail.com>
2023-06-23 14:08:51 +0300

    Update ChatGPT bookmarklet to work in latest UI

--- chatgpt-print-bookmarklet.js
+++ chatgpt-print-bookmarklet.js
@@ -4,9 +4,16 @@
           node.classList.remove('overflow-hidden-off');
           node.classList.add('overflow-hidden');
       });
+      document.querySelectorAll('html *.overflow-auto-off').forEach(function(node) {
+          node.classList.remove('overflow-auto-off');
+          node.classList.add('overflow-auto');
+      });
       document.querySelectorAll('html *.absolute-off').forEach(function(node) {
           node.classList.remove('absolute-off');
           node.classList.add('absolute');
+      });
+      document.querySelectorAll('html .self-end button').forEach(function(node) {
+          node.parentElement.classList.remove('hidden');
       });
       window.PRINTER_FRIENDLY = null;
     } else {
@@ -14,9 +21,16 @@
           node.classList.remove('overflow-hidden');
           node.classList.add('overflow-hidden-off');
       });
+      document.querySelectorAll('html *.overflow-auto').forEach(function(node) {
+          node.classList.remove('overflow-auto');
+          node.classList.add('overflow-auto-off');
+      });
       document.querySelectorAll('html *.absolute').forEach(function(node) {
           node.classList.remove('absolute');
           node.classList.add('absolute-off');
+      });
+      document.querySelectorAll('html .self-end button').forEach(function(node) {
+          node.parentElement.classList.add('hidden');
       });
       window.PRINTER_FRIENDLY = true;
     }
