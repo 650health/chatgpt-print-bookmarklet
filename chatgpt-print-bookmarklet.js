55c0dc9500aaa86f6d7f7d97df1c81d1187f8132
Serge Smetana <smetana@650health.com>
2025-03-31 13:17:18 +0300

    Improve bookmarklet:
    - remove border around deep research results
    - fix bookmarklet to deal with latest changes to input box

--- chatgpt-print-bookmarklet.js
+++ chatgpt-print-bookmarklet.js
@@ -13,7 +13,14 @@
           node.classList.remove('absolute-off');
           node.classList.add('absolute');
       });
+      document.querySelectorAll('html *.isolate').forEach(function(node) {
+          node.style.display = 'flex';
+      });
       document.getElementById("pf-styles").remove();
+      let e = document.getElementsByClassName('sm:p-8');
+      if (e.length > 0) {
+          e[0].removeAttribute("style");
+      }
       window.PF = null;
     } else {
       document.querySelectorAll('html *.overflow-hidden').forEach(function(node) {
@@ -28,12 +35,19 @@
           node.classList.remove('absolute');
           node.classList.add('absolute-off');
       });
+      document.querySelectorAll('html *.isolate').forEach(function(node) {
+          node.style.display = 'none';
+      });
       document.querySelector('div[role=presentation]').classList.remove('flex');
       let styleElement = document.createElement("style");
       styleElement.id = "pf-styles";
       document.head.appendChild(styleElement);
       styleElement.sheet.insertRule("article > div {padding: 0px !important;}", 0);
       styleElement.sheet.insertRule("* { font-size: 12px; line-height: 1.3; margin: 0px !important;}", 0);
+      let e = document.getElementsByClassName('sm:p-8');
+      if (e.length > 0) {
+          e[0].setAttribute("style", "border: none !important; padding: 0px !important;");
+      }
       window.PF = true;
     }
 })();
