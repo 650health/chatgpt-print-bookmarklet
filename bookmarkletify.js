0e015a266701eabe9b57ceea2e22a44690dab6c5
Serge Smetana <smetana@650health.com>
2026-05-15 10:21:26 +0300

    Minify inlined CSS in bookmarkletify

--- bookmarkletify.js
+++ bookmarkletify.js
@@ -1,7 +1,18 @@
-// Copied from https://github.com/t-mart/kill-sticky
+// Based on https://github.com/t-mart/kill-sticky
+// Minifies inlined CSS before URL encoding.
 
 import getStdin from 'get-stdin';
 
 getStdin().then(str => {
+    str = str.replace(/\bcss=`(.*?)`/gs, (_match, css) => {
+        css = css
+            .replace(/\s+/g, ' ')
+            .replace(/\s*([{}:;,>])\s*/g, '$1')
+            .replace(/;}/g, '}')
+            .trim();
+
+        return `css=\`${css}\``;
+    }).trim();
+
     console.log(`{"bookmarklet": "javascript:${encodeURIComponent(str)}"}`);
 });
